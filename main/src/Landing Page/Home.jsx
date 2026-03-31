5571588d5a12e07e02e23b678eae11897a298310
harryhcalendatas-sudo <harryhcalendatas@gmail.com>
2026-03-31 11:58:38 +0800

    Activity 1

---
+++ main/src/Landing Page/Home.jsx
@@ -0,0 +1,20 @@
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <main className="Group9">
+            <h1>BSIT-2A Group 9 Members</h1>
+            <p>This Group needs to be Revamped.</p>
+            <section>
+                <ul>
+                    <li><strong>Member 1:</strong> Sean Benedict C. De Atras</li>
+                    <li><strong>Member 2:</strong> Arlon S. Llacer</li>
+                    <li><strong>Member 3:</strong> Enaro M. Caber</li>
+                    <li><strong>Member 4:</strong> Harry H. Calendatas</li>
+                </ul>
+            </section>
+        </main>
+    );
+}
+
+export default Home;
